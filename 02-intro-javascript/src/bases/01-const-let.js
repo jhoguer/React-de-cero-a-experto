abdd623633aebad4a701a4c1f6129fbe0055bd3f
Jhon Guerrero <jhoguer@gmail.com>
2020-10-08 23:19:53 -0500

    Usando template strings

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+// Variables y constantes
+
+const nombre = 'Jhon';
+let apellido = 'Guerrero';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  const nombre = 'Peter';
+  let valorDado = 6;
+
+  console.log(valorDado);
+  console.log(nombre);
+}
+
+console.log(valorDado);
