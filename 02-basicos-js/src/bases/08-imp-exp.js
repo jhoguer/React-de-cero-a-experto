96a7f4a634e904f8a1a92f3496e936dd4205a70d
Jhon Guerrero <jhoguer@gmail.com>
2020-10-10 21:09:39 -0500

    Aplicando promesas

--- 02-basicos-js/src/bases/08-imp-exp.js
+++ 02-basicos-js/src/bases/08-imp-exp.js
@@ -1,14 +1,11 @@
 import heroes, { owners } from './data/heroes';
 
-console.log( owners );
 
 // find
-const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
+export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-console.log( getHeroeById(3).name );
 
 
 // filter
-const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-console.log(getHeroesByOwner('Marvel'))
