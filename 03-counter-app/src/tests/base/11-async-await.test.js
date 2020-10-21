09db9bdbe87fd69c40d25bc2392c0cfee99b3578
Jhon Guerrero <jhoguer@gmail.com>
2020-10-20 20:05:08 -0500

    Probando tareas asincronas y prubas con async-await

---
+++ 03-counter-app/src/tests/base/11-async-await.test.js
@@ -0,0 +1,13 @@
+const { getImagen } = require("../../base/11-async-await")
+
+
+describe('Pruebas con async-await y fetch', () => {
+
+  test('debe retornar el URL de la imagen', async() => {
+
+    const url = await getImagen();
+    console.log(url);
+
+    expect( url.includes('https://') ).toBe( true );
+  })
+})
