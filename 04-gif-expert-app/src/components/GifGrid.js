96a856f39bafc16dca952e3bf48ff9b7ba4e40c4
Jhon Guerrero <jhoguer@gmail.com>
2020-10-23 23:21:55 -0500

    Agregando clases para los estilos de cada card

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -30,13 +30,16 @@ export const GifGrid = ({ category }) => {
 
 
   return (
-    <div>
-      <h3> { category } </h3>
+    <>
+    <h3> { category } </h3>
+    <div className="card-grid">
         { 
           images.map( ( img ) => (
             <GifGridItem key={ img.id } { ...img } />
           ))
         }
     </div>
+    </>
+    
   )
 }
